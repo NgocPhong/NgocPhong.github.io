87fab4dbe1e957661af3614cf86477806a9a2914
darkraide <aloola.phong@gmail.com>
2014-11-10 01:26:40 +0700

    update in report connection

--- js/controllers/reportcontroller.js
+++ js/controllers/reportcontroller.js
@@ -9,7 +9,7 @@ Schedule.TotalreportController = Ember.Controller.extend({
              var _data = this.getProperties( "fromdate", "todate");
              console.log( _data);
              var seft = this;
-             $.post("http://localhost:8080/api/Project/report",
+             $.post("https://rocky-dusk-5116.herokuapp.com/api/Project/report",
               _data,
               function(data, textStatus, jqXHR)
               {
@@ -25,7 +25,7 @@ Schedule.TotalreportController = Ember.Controller.extend({
    
     var seft = this;
     var _data = this.getProperties( "tfromdate", "ttodate","projectid");
-       $.post("http://localhost:8080/api/Task/report",
+       $.post("https://rocky-dusk-5116.herokuapp.com/api/Task/report",
               _data,
               function(data, textStatus, jqXHR)
               {
@@ -51,7 +51,7 @@ Schedule.TotalreportController = Ember.Controller.extend({
           
         });*/
      },
-  },
+  }
   // Used for horizontal bar chart, vertical bar chart, and pie chart
  /* content: [
   {
@@ -81,84 +81,5 @@ Schedule.TotalreportController = Ember.Controller.extend({
   ],*/
   
  
-  
-  // Used only for time series chart
-  timeSeriesBarContent: [
-    {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-      label: "Financial analytics software",
-      value: 49668
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-      label: "Financial analytics software",
-      value: 68344
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-      label: "Financial analytics software",
-      value: 60654
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-      label: "Financial analytics software",
-      value: 48240
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-      label: "Financial analytics software",
-      value: 62074
-    }
-  ],
-  
-  // Used only for time series chart
-  timeSeriesLineContent: [
-    {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-      label: "Software & Programming",
-      value: 17326,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-      label: "Telecommunication",
-      value: 4515,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-      label: "Software & Programming",
-      value: 15326,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-      label: "Telecommunication",
-      value: 1515,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-      label: "Software & Programming",
-      value: 14326,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-      label: "Telecommunication",
-      value: 8518,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-      label: "Software & Programming",
-      value: 42301,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-      label: "Telecommunication",
-      value: 90191,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-      label: "Software & Programming",
-      value: 57326,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-      label: "Telecommunication",
-      value: 39544,
-      type: "money"
-    }
-  ]
+ 
 });
